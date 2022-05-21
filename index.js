d2adcca7ee1e31be37b07fd2b334e0aec4874c2b
khadiza21 <bibikhadiza474@gmail.com>
2022-05-21 22:54:15 +0600

    mongo connecter

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const express = require("express");
 const cors = require("cors");
-// const jwt = require("jsonwebtoken");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+//const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-
 
 //moddleware
 app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ptgfh8c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const manufacturerCollection = client
+      .db("manufacturer")
+      .collection("tools");
+
+    //get all data of tool items
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = manufacturerCollection.find(query);
+      const tool = await cursor.toArray();
+      res.send(tool);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-    res.send("manufacturer server is runnuning");
-  });
-  app.listen(port, () => {
-    console.log("server is running on port", port);
-  });
+  res.send("manufacturer server is runnuning well");
+});
+app.listen(port, () => {
+  console.log("server is running well on port", port);
+});
