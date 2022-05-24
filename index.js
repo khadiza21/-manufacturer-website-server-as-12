c32e8b1b1c972608ec435e0784a45fe69597fd93
khadiza21 <bibikhadiza474@gmail.com>
2022-05-24 11:33:42 +0600

    more pages add

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ async function run() {
     const manufacturerCollection = client
       .db("manufacturer")
       .collection("tools");
+    const orderCollection = client.db("manufacturer").collection("orders");
+    const reviewCollection = client.db("").collection("reviews");
+    const userCollection = client.db('manufacturer').collection('users');
 
     //get all data of tool items
     app.get("/tools", async (req, res) => {
@@ -32,6 +35,20 @@ async function run() {
       res.send(tool);
     });
 
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      //const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
+    });
+
     //find user for details
     app.get("/tools/:id", async (req, res) => {
       const id = req.params.id;
@@ -39,6 +56,38 @@ async function run() {
       const result = await manufacturerCollection.findOne(query);
       res.send(result);
     });
+
+
+
+    app.get("/orders", async (req, res) => {
+      const query = {};   
+      const cursor = orderCollection.find(query); 
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    //POST
+    app.post("/orders", async (req, res) => {
+      const newOrder = req.body;
+      const result = await orderCollection.insertOne(newOrder);
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const review = await cursor.toArray();
+      res.send(review);
+    });
+
+     //POST
+     app.post("/reviews", async (req, res) => {
+      const newReview = req.body;
+      const result = await reviewCollection.insertOne(newReview);
+      res.send(result);
+    });
+
+
   } finally {
   }
 }
